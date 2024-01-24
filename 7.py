68055264978ed06a1ca8de00f36bc06b09256dea
ZhAkerke <157611390+ZhAkerke@users.noreply.github.com>
2024-01-24 20:25:15 +0600

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,15 @@
+#The Python print() function is often used to output variables.
+x = "Python is awesome"
+print(x)
+
+#In the print() function, you output multiple variables, separated by a comma:
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+#You can also use the + operator to output multiple variables:
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
